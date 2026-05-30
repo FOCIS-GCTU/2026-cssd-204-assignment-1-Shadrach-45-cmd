eb69bce818eaf3365416f89cf70d8cdd17e30e7f
Shadrach-45-cmd <2425403229@live.gctu.edu.gh>
2026-05-30 20:52:33 +0000

    Add files via upload

--- initials.py
+++ initials.py
@@ -1,7 +1,32 @@
+# File: initials.py
+# Description: Print out my initials.
+# Assignment Number: 1
+#
+# Name: Shadrach Asante Larweh
+# STUDENT ID: 2425403229
+# Email: 2425403229@live.gctu.edu.gh
+# Grader: Augustus Buckman
+#
+# On my honor, Shadrach, this programming assignment is my own work
+# and I have not provided this code to any other student.
 
-def main():
-  pass #code goes here
+
+def main(   ):
+    """Display the initials S, A, L as large block letters."""
+    print()
+    print("...SAL")
+    print()
+    print("...  SSSSSSSS  ...     AA     ...LL          ...")
+    print("... SS      SS ...    AAAA    ...LL          ...")
+    print("...SS          ...   AA  AA   ...LL          ...")
+    print("...SS          ...  AA    AA  ...LL          ...")
+    print("...  SSSSSSSS  ... AA      AA ...LL          ...")
+    print("...          SS... AAAAAAAAAA ...LL          ...")
+    print("...          SS...AA        AA...LL          ...")
+    print("...SS        SS...AA        AA...LL          ...")
+    print("... SS      SS ...AA        AA...LL          ...          **")
+    print("...  SSSSSSSS  ...AA        AA...LLLLLLLLLLLL...          **")
+    print()
 
 
-
-main()
+main(   )
